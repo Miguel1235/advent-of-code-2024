2007e0614eeee0977cdea8dddc46753614509614
Miguel Del Corso <migueldelcorso@gmail.com>
2024-12-01 10:14:13 -0300

    First day completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,21 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val part1 = { input: List<Pair<Int, Int>> ->  input.sumOf {(lv, rv) -> abs(lv-rv)} }
+    val part2 = { input: List<Pair<Int, Int>> -> input.sumOf { (lv, _) -> lv * input.count { it.second == lv } }}
+
+    fun parseInput(input: List<String>): List<Pair<Int, Int>> {
+        val (l,r) = input
+            .map { line -> line.split(" ").filter { it.isNotEmpty() } }.map { Pair(it[0].toInt(), it[1].toInt()) }
+            .unzip()
+        return l.sorted().zip(r.sorted())
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val testInput = parseInput(readInput("Day01_test"))
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val input = parseInput(readInput("Day01"))
+    check(part1(input) == 2066446)
+    check(part2(input) == 24931009)
 }
