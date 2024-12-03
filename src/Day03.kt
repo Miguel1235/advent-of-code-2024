8f86956a179f7977cfd78e1dc33cab5bad233ad2
Miguel Del Corso <migueldelcorso@gmail.com>
2024-12-03 10:22:56 -0300

    More changes on Day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,45 +1,34 @@
-fun part1(memory: String): Long {
-    val memoryRegex = Regex("""mul\(\d+,\d+\)""")
-    val instructions = memoryRegex.findAll(memory)
-
-    val numbersRegex = Regex("""\d+""")
-    val numbers = instructions.map { instruction ->
-        val matches = numbersRegex.findAll(instruction.value)
-        matches.map { it.value.toLong() }.toList()
-    }.toList()
-
-    return numbers.fold(0L) { acc, (f, s) -> acc + (f * s) }
-}
-
-fun part2(memory: String): Long {
-    val memoryRegex = Regex("""mul\(\d+,\d+\)|don't\(\)|do\(\)""")
-    val instructions = memoryRegex.findAll(memory)
+fun part(memory: String, withDos: Boolean = false): Long {
+    val memoryRegex = Regex("""mul\((\d+),(\d+)\)|don't\(\)|do\(\)""")
     var isMulEnabled = true
-    val numbersRegex = Regex("""\d+""")
 
-    val numbers = mutableListOf<List<Long>>()
-    instructions.forEach { instruction ->
-        val r = instruction.value
+    return memoryRegex.findAll(memory).sumOf { instruction ->
+        when (val r = instruction.value) {
+            "don't()" -> {
+                if (withDos) isMulEnabled = false
+                0L
+            }
+            "do()" -> {
+                if (withDos) isMulEnabled = true
+                0L
+            }
 
-        when {
-            r.contains("don't()") -> isMulEnabled = false
-            r.contains("do()") -> isMulEnabled = true
-            r.contains("mul") && isMulEnabled -> {
-                val matches = numbersRegex.findAll(r)
-                numbers.add(matches.map { it.value.toLong() }.toList())
+            else -> {
+                if (isMulEnabled && r.startsWith("mul")) {
+                    val (f, s) = Regex("""\d+""").findAll(r).map { it.value.toLong() }.toList()
+                    f * s
+                } else 0L
             }
         }
     }
-
-    return numbers.fold(0L) { acc, (f, s) -> acc + (f * s) }
 }
 
 fun main() {
     val testInput = readOneLine("Day03_test")
-    check(part1(testInput) == 161L)
-    check(part2(testInput) == 48L)
+    check(part(testInput) == 161L)
+    check(part(testInput, true) == 48L)
 
     val input = readOneLine("Day03")
-    check(part1(input) == 189600467L)
-    check(part2(input) == 107069718L)
+    check(part(input) == 189600467L)
+    check(part(input, true) == 107069718L)
 }
