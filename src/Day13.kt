7df4445e71f112166d761f5ce32a42243d33a847
Miguel Del Corso <migueldelcorso@gmail.com>
2024-12-13 11:58:51 -0300

    Change the regular functions for lambda ones

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,11 +1,20 @@
 import kotlin.math.abs
 
-private fun part1(input: List<String>, part1: Boolean = true): ULong {
+private val obtainValues = { regex: Regex, input: String -> regex.find(input)?.groupValues!!.takeLast(2).map { it.toDouble() } }
+
+private val calculateCost = { deltaXA: Double, deltaYA: Double, deltaXB: Double, deltaYB: Double, x: Double, y: Double ->
+    // crammer rule
+    val determinant = deltaXA * deltaYB - deltaXB * deltaYA
+    val a = abs((x * deltaYB - y * deltaXB) / determinant)
+    val b = abs((x * deltaYA - y * deltaXA) / determinant)
+    if (a.rem(1.0) == 0.0 && b.rem(1.0) == 0.0) (3u * a.toULong() + b.toULong()) else 0u
+}
+
+private val part = { input: List<String>, part1: Boolean ->
     val regexXY = Regex("""X\+(\d+), Y\+(\d+)""")
     val prizeRegex = Regex("""X=(\d+), Y=(\d+)""")
     val prizeOffset = 10000000000000
-
-    return input.chunked(4).sumOf { (i1, i2, i3, _) ->
+    input.chunked(4).sumOf { (i1, i2, i3, _) ->
         val (xa, ya) = obtainValues(regexXY, i1)
         val (xb, yb) = obtainValues(regexXY, i2)
         val (xp, yp) = obtainValues(prizeRegex, i3)
@@ -13,34 +22,13 @@ private fun part1(input: List<String>, part1: Boolean = true): ULong {
     }
 }
 
-val obtainValues = { regex: Regex, input: String -> regex.find(input)?.groupValues!!.takeLast(2).map { it.toDouble() } }
-
-private fun calculateCost(
-    deltaXA: Double,
-    deltaYA: Double,
-    deltaXB: Double,
-    deltaYB: Double,
-    x: Double,
-    y: Double
-): ULong {
-    // crammer
-    val determinant = deltaXA * deltaYB - deltaXB * deltaYA
-    val a = abs((x * deltaYB - y * deltaXB) / determinant)
-    val b = abs((x * deltaYA - y * deltaXA) / determinant)
-
-    return if (a.rem(1.0) == 0.0 && b.rem(1.0) == 0.0) {
-        (3u * a.toULong() + b.toULong())
-    } else 0u
-}
-
 fun main() {
     val testInput = readInput("Day13_test")
-    part1(testInput, true)
-    check(part1(testInput) == 480.toULong())
-    check(part1(testInput, false) == 875318608908.toULong())
+    check(part(testInput, true) == 480.toULong())
+    check(part(testInput, false) == 875318608908.toULong())
 
     val input = readInput("Day13")
-    check(part1(input) == 29598.toULong())
-    check(part1(input, false) == 93217456941970.toULong())
+    check(part(input, true) == 29598.toULong())
+    check(part(input, false) == 93217456941970.toULong())
 }
  
