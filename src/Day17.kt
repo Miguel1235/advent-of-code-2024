75054135091cb4ca443d330ec9323b9abc704883
Miguel Del Corso <migueldelcorso@gmail.com>
2024-12-17 12:39:39 -0300

    Made some quick improves

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,85 +1,49 @@
 import kotlin.math.pow
 
-data class Reg(val name: Char, var value: Long = 0) {
-    override fun toString() = "$name:$value"
-}
+//data class Reg(val name: Char, var value: Long = 0) {
+//    override fun toString() = "$name:$value"
+//}
 
 private enum class Opcode { ADV, BXL, BST, JNZ, BXC, OUT, BDV, CDV }
 
-private fun part1(input: List<String>): String {
-    val program = input.last().split(":").last().trim().split(",").chunked(2).map { ins ->
-            Pair(Opcode.entries.first { it.ordinal == ins[0].toInt() }, ins[1].toInt())
-        }
+private fun getDenominator(operand: Int, a: Long, b: Long, c: Long) =
+    (2.0.pow(getComboValue(operand.toLong(), a,b,c).toDouble())).toLong()
 
-    val (a, b, c) = input.take(3).map { it.split(":").last().trim().toLong() }.mapIndexed { i, v ->
-        Reg(
-            when (i) {
-                0 -> 'A'
-                1 -> 'B'
-                else -> 'C'
-            }, v
-        )
-    }
+private fun getComboValue(operand: Long, a: Long, b: Long, c: Long): Long {
+    return when (operand) {
+            1L,2L,3L -> operand
+            4L -> a
+            5L -> b
+            else -> c
+        }
+}
+
+
+private fun part1(input: List<String>): String {
+    val program = input.last().split(":").last().trim().split(",").chunked(2).map { ins -> Pair(Opcode.entries.first { it.ordinal == ins[0].toInt() }, ins[1].toInt()) }
+    var (a, b, c) = input.take(3).map { it.split(":").last().trim().toLong() }
 
     val output = mutableListOf<Long>()
     var instructionPointer = 0
-
     while (instructionPointer < program.size) {
         val (instruction, operand) = program[instructionPointer]
-
         when (instruction) {
-            Opcode.ADV -> {
-                a.value /= getDenominator(operand, a, b, c)
-            }
-
-            Opcode.BXL -> {
-                b.value = b.value.xor(operand.toLong())
-            }
-
-            Opcode.BST -> {
-                b.value = getComboValue(operand.toLong(), listOf(a, b, c)) % 8
-            }
-
+            Opcode.ADV -> a /= getDenominator(operand, a, b, c)
+            Opcode.BXL -> b = b.xor(operand.toLong())
+            Opcode.BST -> b = getComboValue(operand.toLong(), a, b, c) % 8
+            Opcode.BXC -> b = b.xor(c)
+            Opcode.OUT -> output.add(getComboValue(operand.toLong(), a, b, c) % 8)
+            Opcode.BDV -> b = a / getDenominator(operand, a, b, c)
+            Opcode.CDV -> c = a / getDenominator(operand, a, b, c)
             Opcode.JNZ -> {
-                if (a.value == 0L) break
+                if (a == 0L) break
                 instructionPointer = operand
                 continue
-            }
-
-            Opcode.BXC -> {
-                b.value = b.value.xor(c.value)
-            }
-
-            Opcode.OUT -> {
-                output.add(getComboValue(operand.toLong(), listOf(a, b, c)) % 8)
-            }
-
-            Opcode.BDV -> {
-                b.value = a.value / getDenominator(operand, a, b, c)
-            }
-
-            Opcode.CDV -> {
-                c.value = a.value / getDenominator(operand, a, b, c)
             }
         }
         instructionPointer++
     }
     return output.joinToString(separator = ",")
-}
-
-private fun getDenominator(operand: Int, a: Reg, b: Reg, c: Reg) =
-    (2.0.pow(getComboValue(operand.toLong(), listOf(a, b, c)).toDouble())).toLong()
-
-private fun getComboValue(operand: Long, regs: List<Reg>): Long {
-    return if (operand <= 3) {
-        operand
-    } else {
-        when (operand) {
-            4L -> regs[0].value
-            5L -> regs[1].value
-            else -> regs[2].value
-        }
-    }
 }
 
 private fun part2(input: List<String>): Int {
@@ -88,6 +52,7 @@ private fun part2(input: List<String>): Int {
 
 fun main() {
     val testInput = readInput("Day17_test")
+
     check(part1(testInput) == "4,2,5,6,7,7,7,7,3,1,0")
 //    check(part2(testInput) == 0)
 
