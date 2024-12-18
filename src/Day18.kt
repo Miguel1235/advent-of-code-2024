539cdf94e0e80ad8d83699d33af359d99ef1c32c
Miguel Del Corso <migueldelcorso@gmail.com>
2024-12-18 12:43:00 -0300

    Made some changes to make it more kotlish

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,37 +1,28 @@
-private class Gh(
-    private val map: List<List<Vertex>>,
-) {
+private class Gh(private val map: List<List<Vertex>>) {
     val directions = listOf(
-        Pair(0,-1),
-        Pair(1,0),
-        Pair(0,1),
-        Pair(-1,0)
+        Pair(0, -1), Pair(1, 0), Pair(0, 1), Pair(-1, 0)
     )
 
-    val nodes: Map<Vertex, List<Vertex>> = buildMap {
-        for (r in map.indices) {
-            for (c in map[r].indices) {
-                val node = map[r][c]
-                val nbs = mutableListOf<Vertex>()
-                for ((dx, dy) in directions) {
-                    val nb = map.getOrNull(r + dx)?.getOrNull(c + dy)
-                    if (nb != null) nbs.add(nb)
-                }
-                put(node, nbs.filter { it.name != '#' })
+    val nodes = buildMap {
+        map.forEachIndexed { r, row ->
+            row.forEachIndexed { c, node ->
+                val neighbors = directions.mapNotNull { (dx, dy) -> map.getOrNull(r + dx)?.getOrNull(c + dy) }
+                    .filter { it.name != '#' }
+                put(node, neighbors)
             }
         }
     }
 
     fun bfs(start: Vertex, end: Vertex): List<Vertex> {
-        val visited =  mutableSetOf<Vertex>()
+        val visited = mutableSetOf<Vertex>()
         val queue: ArrayDeque<List<Vertex>> = ArrayDeque() // Stores paths
         queue.add(listOf(start))
-        while(queue.isNotEmpty()) {
+        while (queue.isNotEmpty()) {
             val path = queue.removeFirst()
             val node = path.last()
 
             if (node == end) return path
-            if(node !in visited) {
+            if (node !in visited) {
                 visited.add(node)
                 nodes[node]?.forEach { neighbor ->
                     queue.addLast(path + neighbor)
@@ -46,60 +37,38 @@ private data class Vertex(val name: Char, val position: Pair<Int, Int>) {
     override fun toString() = "$name"
 }
 
-
 private fun part1(input: List<String>, size: Int = 70, bytes: Int = 1024): Int {
-    val reducedBytes = input.map {
-        val (r, c) = it.split(",").map { it.toInt() }
-        Pair(r,c)
-    }.take(bytes)
-
-    val grid = obtainGrid(size, reducedBytes)
-    val gh = Gh(grid)
-
-    return gh.bfs(grid[0][0], grid[size][size]).size - 1
+    val reducedBytes = reduceBytes(input)
+    val grid = obtainGrid(size, reducedBytes.take(bytes))
+    return Gh(grid).bfs(grid[0][0], grid[size][size]).size - 1
 }
 
-
-private fun prettyPrint(map: List<List<Vertex>>) {
-    for(r in map.indices) {
-        for(c in map[r].indices) {
-            print(map[r][c])
-        }
-        println()
+val reduceBytes = { input: List<String> ->
+    input.map {
+        val (r, c) = it.split(",").map { it.toInt() }
+        Pair(r, c)
     }
-    println()
 }
 
 private fun part2(input: List<String>, size: Int = 70): Pair<Int, Int> {
-    val reducedBytes = input.map {
-        val (r, c) = it.split(",").map { it.toInt() }
-        Pair(r,c)
-    }
-
+    val reducedBytes = reduceBytes(input)
     var total = 1
     try {
-        while(true) {
+        while (true) {
             val grid = obtainGrid(size, reducedBytes.take(total))
-            val gh = Gh(grid)
-
-            gh.bfs(grid[0][0], grid[size][size]).size - 1
+            Gh(grid).bfs(grid[0][0], grid[size][size]).size - 1
             total++
         }
     } catch (e: Exception) {
-        return reducedBytes[total-1]
+        return reducedBytes[total - 1]
     }
 }
 
-private fun obtainGrid(size: Int, reducedBytes: List<Pair<Int, Int>>): List<List<Vertex>> {
-    return buildList {
-        for (r in 0..size) {
-            val row = buildList {
-                for (c in 0..size) {
-                    val position = Pair(c, r)
-                    add(Vertex(if (reducedBytes.contains(position)) '#' else '.', position))
-                }
-            }
-            add(row)
+private val obtainGrid = { size: Int, reducedBytes: List<Pair<Int, Int>> ->
+    List(size + 1) { r ->
+        List(size + 1) { c ->
+            val position = c to r
+            Vertex(if (position in reducedBytes) '#' else '.', position)
         }
     }
 }
@@ -107,10 +76,10 @@ private fun obtainGrid(size: Int, reducedBytes: List<Pair<Int, Int>>): List<List
 fun main() {
     val testInput = readInput("Day18_test")
     check(part1(testInput, 6, 12) == 22)
-    check(part2(testInput, 6) == Pair(6,1))
+    check(part2(testInput, 6) == Pair(6, 1))
 
     val input = readInput("Day18")
     check(part1(input) == 372)
-    check(part2(input) == Pair(25,6))
+    check(part2(input) == Pair(25, 6))
 }
  
